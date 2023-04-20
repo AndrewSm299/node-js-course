b373869d67085edd5408586b47e387a09f45c73e
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-04-20 12:55:22 +0300

    Update OOP.js
    ор

--- 1.1 JS Basics/OOP.js
+++ 1.1 JS Basics/OOP.js
@@ -1,1 +1,16 @@
-
+class AbstractProduct {
+constructor(id, name, description, price, quantity, reviews, images, date, brand) {
+this.id = id
+this.name name
+this.description = description
+this.price = price
+this.quantity quantity
+this.reviews = reviews
+this.images images
+this.date = date
+this.brand
+brand
+// methods
+getFullInformation() {
+throw new Error('Subclass must implement abstract method.');
+}
