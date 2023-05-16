926c1566857890eb9eba61ef37aba7cee6ac8399
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-05-16 18:42:45 +0200

    Update Data types & operators. Objects and functions.js

--- Level 1/1.1 JS Basics/Data types & operators. Objects and functions.js
+++ Level 1/1.1 JS Basics/Data types & operators. Objects and functions.js
@@ -1,15 +1,15 @@
 /**
  *
- * @param {String} id of  the product
- * @param {String} name of the product
- * @param {String} description of the product
- * @param {Number} price of the product
- * @param {String} brand of the product
- * @param {Array.<string>} sizes of the product
- * @param {String} activesizes of the product
- * @param {Integer} quantity of the product
- * @param {Array.<object>} reviews on the product
- * @param {Array.<string>} images of the products
+ * @param {String} id id of  the product
+ * @param {String} name name of the product
+ * @param {String} description description of the product
+ * @param {Number} price price of the product
+ * @param {String} brand brand of the product
+ * @param {Array.<string>} sizes sizes of the product
+ * @param {String} activesizes active sizes of the product
+ * @param {Integer} quantity quantity of the product
+ * @param {Array.<object>} reviews reviews on the product
+ * @param {Array.<string>} images images of the products
  *
 */
 
@@ -52,8 +52,8 @@ function Product(id, name, description, price, brand, sizes, activesize, quantit
     this.setSizes = (sizes) => this.sizes = sizes
 
 
-    this.getActiveSizes = () => this.activesizes
-    this.setActiveSizes = (activesizes) => this.activesizes = activesizes
+    this.getActiveSize = () => this.activesize
+    this.setActiveSize = (activesize) => this.activesize = activesize
 
 
     this.getQuantity = () => this.quantity
@@ -85,51 +85,56 @@ function Product(id, name, description, price, brand, sizes, activesize, quantit
     },
 
 
-    this.addSize = (size_new) => this.sizes = sizes.push(size_new)
-   
-    this.deleteSize = (size_key) => this.sizes.splice(IndexOf(this.sizes.find(e => e === size_key)), 1)
-
-
+    this.addSize = (newsize) => {
+        if(Array.isArray(this.sizes)) {
+            this.sizes.push(newsize)
+        }
+        else{
+            this.sizes = [newsize]
+        }
+    }
+    
+    this.deleteSize = (size_key) => {
+        if (Array.isArray(this.sizes)) {
+          const index = this.sizes.findIndex((size) => size === size_key)
+          if (index !== -1) {
+            this.sizes.splice(index, 1);
+          }
+        else{
+            throw new Error('Enter index to delete size')
+        }
+        }
+    }
 
     this.addReview = (newreview) => this.reviews.push(newreview)
 
-
-    this.deleteReview = (rev_id) => this.reviews.splice(IndexOf(this.reviews.find(e => e === rev_id)), 1)
-
+    this.deleteReview = (rev_id) => this.reviews.splice(this.reviews.findIndex((review) => review.id === rev_id), 1)
 
     this.getAverageRating = () => {
-        averagerating = {service: 0, price: 0, value: 0, quality: 0}
-        let a = 0
-        for(let i = 0; i < this.reviews.rating.length; i++){
-            a += this.reviews.rating.service[i]
+        if (this.reviews.length === 0) {
+          return null
         }
-        averagerating.service = a / this.reviews.rating.length
-        a = 0
-
-
-        for(let i = 0; i < this.reviews.rating.length; i++){
-            a += this.reviews.rating.price[i]
+      
+        const totalRatings = this.reviews.reduce(
+          (acc, review) => {
+            acc.service += review.rating[0];
+            acc.price += review.rating[1];
+            acc.value += review.rating[2];
+            acc.quality += review.rating[3];
+            return acc;
+          },
+          { service: 0, price: 0, value: 0, quality: 0 }
+        )
+      
+        const averageRatings = {
+          service: totalRatings.service / this.reviews.length,
+          price: totalRatings.price / this.reviews.length,
+          value: totalRatings.value / this.reviews.length,
+          quality: totalRatings.quality / this.reviews.length,
         }
-        averagerating.price = a / this.reviews.rating.length
-        a = 0
-
-
-        for(let i = 0; i < this.reviews.rating.length; i++){
-            a += this.reviews.rating.value[i]
-        }
-        averagerating.value = a / this.reviews.rating.length
-        a = 0
-
-
-        for(let i = 0; i < this.reviews.rating.length; i++){
-            a += this.reviews.rating.quality[i]
-        }
-        averagerating.quality = a / this.reviews.rating.length
-
-
-        return averagerating
+      
+        return averageRatings
     }
-
 
 }
 
@@ -168,7 +173,7 @@ function Reviews(r_ID, author, date, comment, rating){
     this.getComment = () => this.comment
 
 
-    this.setRating = (rating) => this.rating = {'service': rating[0], 'price': rating[1], 'value': rating[2], 'quality': rating[3]}
+    this.setRating = (rating) => this.rating = {service: rating[0], price: rating[1], value: rating[2], quality: rating[3]}
     this.getRating = () => this.rating
 }
 
@@ -181,7 +186,8 @@ function Reviews(r_ID, author, date, comment, rating){
 
 
 function searchProducts (products, search) {
-    let searchresult = [];
+    let searchresult = []
+    search = search.trim().toLowerCase()
     products.forEach((element) => {
         let str = "";
         str = element.name + " " + element.description;
@@ -230,3 +236,90 @@ function sortrule (products, sortRule) {
             break;
    }
 }
+
+// TESTS
+
+const product = new Product("123","Product Name","Product Description",10.99,"Brand",["S", "M", "L"],"M",50,"2023-05-16",["image1.jpg", "image2.jpg"],[])
+
+// getters
+console.log(product.getID()) // Output: 123
+console.log(product.getName()) // Output: Product Name
+console.log(product.getDescription()) // Output: Product Description
+console.log(product.getPrice()) // Output: 10.99
+console.log(product.getBrand()) // Output: Brand
+console.log(product.getSizes()) // Output: ["S", "M", "L"]
+console.log(product.getActiveSize()) // Output: M
+console.log(product.getQuantity()) // Output: 50
+console.log(product.getDate()) // Output: 2023-05-16
+console.log(product.getImages()) // Output: ["image1.jpg", "image2.jpg"]
+console.log(product.getReviews()) // Output: []
+
+//setters
+
+product.setID("456");
+console.log(product.getID()); // 456
+
+product.setName("New Product Name");
+console.log(product.getName()); // New Product Name
+
+product.setDescription("New Product Description");
+console.log(product.getDescription()); // New Product Description
+
+product.setPrice(19.99);
+console.log(product.getPrice()); // 19.99
+
+product.setBrand("New Brand");
+console.log(product.getBrand()); // New Brand
+
+product.setSizes(["M", "L"]);
+console.log(product.getSizes()); // ["M", "L"]
+
+product.setActiveSize("L");
+console.log(product.getActiveSize()); // L
+
+product.setQuantity(100);
+console.log(product.getQuantity()); // 100
+
+product.setDate("2023-05-17");
+console.log(product.getDate()); // 2023-05-17
+
+product.setImages(["image3.jpg", "image4.jpg"]);
+console.log(product.getImages()); // ["image3.jpg", "image4.jpg"]
+
+product.setReviews([{ id: "r1", author: "John", comment: "Great product!", rating: [5, 4, 4, 5] }]);
+console.log(product.getReviews()); // [{ id: "r1", author: "John", comment: "Great product!", rating: [5, 4, 4, 5] }]
+
+// add/delete size
+product.addSize("XL")
+console.log(product.getSizes()) // ["M", "L", "XL"]
+
+product.deleteSize("M")
+console.log(product.getSizes()) // ["L", "XL"]
+
+// working with reviews
+
+const review1 = new Reviews("r2", "John", "2023-05-16", "Great product!", [5, 4, 4, 5])
+const review2 = new Reviews("r3", "Jane", "2023-05-17", "Not satisfied.", [2, 3, 2, 2])
+
+product.addReview(review1)
+product.addReview(review2)
+console.log(product.getReviews()) // [review1, review2]
+
+product.deleteReview("r1")
+console.log(product.getReviews()) // [review2]
+
+console.log(product.getAverageRating()) // {service: 3.5, price: 3.5, value: 3, quality: 3.5}
+  
+// search/sort product
+
+const products = [
+    new Product("1", "Product A", "Description A", 10.99, "Brand A", ["S", "M"], "M", 50, "2023-05-16", [], []),
+    new Product("2", "Product B", "Description B", 19.99, "Brand B", ["M", "L"], "M", 30, "2023-05-17", [], []),
+    new Product("3", "Product C", "Description C", 8.99, "Brand C", ["S", "L"], "S", 20, "2023-05-18", [], [])]
+
+console.log(searchProducts(products, "product")) // Output: [product A, product B, product C]
+console.log(searchProducts(products, "description b")) // Output: [product B]
+
+console.log(sortrule(products, "name")) // Output: [product A, product B, product C]
+console.log(sortrule(products, "id")) // Output: [product 1, product 2, product 3]
+console.log(sortrule(products, "price")) // Output: [product C, product A, product B]
