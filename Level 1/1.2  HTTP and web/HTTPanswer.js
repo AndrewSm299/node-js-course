0c0ae0e0be41bfcd64c4f3bdd80c89a3c713f631
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-06-19 20:55:31 +0200

    Create HTTPanswer.js

---
+++ Level 1/1.2  HTTP and web/HTTPanswer.js
@@ -0,0 +1,88 @@
+// этот файл надо будет дописать...
+
+// не обращайте на эту функцию внимания 
+// она нужна для того чтобы правильно читать входные данные
+function readHttpLikeInput(){
+    var fs = require("fs");
+    var res = "";
+    var buffer = Buffer.alloc ? Buffer.alloc(1) : new Buffer(1);
+    let was10 = 0;
+    for(;;){ 
+        try { fs.readSync(0 /*stdin fd*/, buffer, 0, 1); } catch (e) {break; /* windows */}
+        if(buffer[0] === 10 || buffer[0] === 13) {
+            if (was10 > 10) 
+                break;
+            was10++;
+        } else 
+           was10 = 0;
+        res += new String(buffer);
+    }
+
+    return res;
+}
+
+let contents = readHttpLikeInput();
+
+function outputHttpResponse(statusCode, statusMessage, headers, body) {
+    const date = new Date(),
+    options = { weekday: 'short', day: '2-digit', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' },
+    formattedDate = date.toLocaleString('en-US', options)
+    console.log(`HTTP/1.1 ${statusCode} ${statusMessage}
+Date: ${formattedDate}
+Server: Apache/2.2.14 (Win32)
+Connection: Closed
+Content-Type: text/html; charset=utf-8
+Content-Length: ${body.length}
+
+${body}`)
+}
+   
+function processHttpRequest($method, $uri, $headers, $body) {
+    if($method == 'GET' && $uri.match(/\/sum\?nums\=[[0-9]*,]*/)){
+        $uri = $uri.slice($uri.indexOf('=') + 1, $uri.length)
+        let arrayOfNumbers = $uri.split(',').map(Number)
+        let sum = 0
+        arrayOfNumbers.forEach(num => sum += num)
+        
+        outputHttpResponse('200','OK' , $headers, sum.toString())
+    }
+    else if($method != 'GET'|| $uri.matches(/\/sum(?!.*\?nums=).*/) ){
+        $body = 'bad request'
+        outputHttpResponse('400', 'Bad Request' , $headers, $body)
+    }
+    else{
+        $body = 'not found'
+        outputHttpResponse('404', 'Not Found', $headers, $body)
+}
+}
+
+function parseTcpStringAsHttpRequest(string) {
+    let array = string.split(/\r?\n/).filter((str) => str.replaceAll(' ', '').length != 0),
+        methodData = array[0].match(/[A-Z]*\s/)[0].trim(),
+        uriData = array[0].match(/\s[^\s]*\s/)[0].trim(),
+        headersData = {},
+        bodyData = array[array.length - 1].trim(),
+        headerName,
+        headerValue,
+        headerParts
+    
+    for (let i = 1; i < array.length - 1; i++) {
+        headerParts = array[i].split(':')
+        headerName = headerParts[0].trim()
+        headerValue = headerParts[1].trim()
+        headersData[headerName] = headerValue
+    }
+
+    return {
+      method: methodData,
+      uri: uriData,
+      headers: headersData,
+      body: bodyData,
+    }
+}
+
+http = parseTcpStringAsHttpRequest(contents);
+processHttpRequest(http.method, http.uri, http.headers, http.body);
+
+
+// for tests of bad request(6 - if method not get, 7 - if no ?nums=) or not found(8 - if no /sum) write node tester.js (number of the test) HTTPanswer.js
