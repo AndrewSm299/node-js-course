f2124122d4feefd1480ffb5bc5818786c0f43cc5
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-11-05 18:45:47 +0100

    Update server.js

--- Level-2/2.3/UDP/server.js
+++ Level-2/2.3/UDP/server.js
@@ -1,1 +1,33 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
+const PORT = 3333;
+
+server.on('error', (err) => {
+    console.error(`Server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+    const currentTime = new Date().toLocaleString();
+    console.log(`${currentTime} ${msg} from ${rinfo.address}:${rinfo.port}`);
+    const responseMessage = msg;
+    server.send(responseMessage, rinfo.port, rinfo.address, (err) => {
+        if (err) {
+            console.error(`${currentTime} Error sending response to client: ${err}`);
+        }
+        else {
+            console.log(`${currentTime} Response sent to ${rinfo.address}:${rinfo.port}: ${responseMessage}`);
+        }
+    });
+});
+
+
+server.on('listening', () => {
+    const address = server.address();
+    const currentTime = new Date().toLocaleString();
+
+    console.log(`${currentTime} UDP Server listening on ${address.address}:${address.port}`);
+});
+
+server.bind(PORT);
