1c6eab738cdaf3d2aa617b7a44009280b714af75
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-11-05 17:06:12 +0100

    Update server.js

--- Level-2/2.3/server.js
+++ Level-2/2.3/server.js
@@ -1,1 +1,27 @@
+const http = require('http');
 
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let requestData = '';
+
+    req.on('data', (chunk) => {
+      requestData += chunk;
+    });
+
+    req.on('end', () => {
+      console.log('Received data from the client:', requestData);
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(requestData);
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
