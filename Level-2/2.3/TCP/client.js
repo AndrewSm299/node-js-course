0b5bc0f456921e005aaf0545b972b5e4bfc89896
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-11-05 18:21:12 +0100

    Update client.js

--- Level-2/2.3/TCP/client.js
+++ Level-2/2.3/TCP/client.js
@@ -1,1 +1,19 @@
+const net = require('net');
 
+const client = new net.Socket();
+
+const PORT = 8080;
+const HOST = '127.0.0.1';
+
+client.connect(PORT, HOST, () => {
+  console.log('Connected to server');
+  client.write('Text to send here');
+});
+
+client.on('data', (data) => {
+  console.log(`Received data from server: ${data}`);
+});
+
+client.on('close', () => {
+  console.log('Connection to server closed');
+});
