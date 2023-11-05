4228a4b7596c0993ccf52258e190a41133a4a8f3
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-11-05 19:00:15 +0100

    Update client.js

--- Level-2/2.3/HTTP/client.js
+++ Level-2/2.3/HTTP/client.js
@@ -12,8 +12,12 @@ const options = {
 
 const startTime = Date.now();
 
+function currentTime(){
+    return new Date().toLocaleString();
+}
+
 const req = http.request(options, (res) => {
-    let data = 'Some text to send to the server';
+    let data = 'Text to send to the server';
 
     res.on('data', (chunk) => {
         data += chunk;
@@ -22,13 +26,13 @@ const req = http.request(options, (res) => {
     res.on('end', () => {
         const endTime = Date.now();
 
-        console.log('Response from server:', data);
-        console.log('Time elapsed (ms):', endTime - startTime);
+        console.log(`${currentTime()} Response from server: ${data}`);
+        console.log(`${currentTime()} Time elapsed (ms): ${endTime - startTime}`);
     });
 });
 
 req.on('error', (error) => {
-    console.error('Request error:', error);
+    console.error(`${currentTime()} Request error: ${error}`);
 });
 
 req.end();
