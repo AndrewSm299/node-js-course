566cf058f2251961b702402ba131b2f707c5d02e
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-11-05 18:51:51 +0100

    Update client.js

--- Level-2/2.3/TCP/client.js
+++ Level-2/2.3/TCP/client.js
@@ -5,19 +5,19 @@ const client = new net.Socket();
 const PORT = 8080;
 const HOST = '127.0.0.1';
 
-function newCurrentTime(){
+function currentTime(){
   return new Date().toLocaleString();
 }
 
 client.connect(PORT, HOST, () => {
-  console.log(`${newCurrentTime()} Connected to server`);
-  client.write(`${newCurrentTime()} Text to send here`);
+  console.log(`${currentTime()} Connected to server`);
+  client.write(`${currentTime()} Text to send here`);
 });
 
 client.on('data', (data) => {
-  console.log(`${newCurrentTime()} Received data from server: ${data}`);
+  console.log(`${currentTime()} Received data from server: ${data}`);
 });
 
 client.on('close', () => {
-  console.log(`${newCurrentTime()} Connection to server closed`);
+  console.log(`${currentTime()} Connection to server closed`);
 });
