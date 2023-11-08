34551b7ae8d26959702e99084fb787a8d6d4a0ea
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-11-08 23:57:58 +0100

    Update client.js

--- Level-2/2.3/UDP/client.js
+++ Level-2/2.3/UDP/client.js
@@ -4,20 +4,22 @@ const client = dgram.createSocket('udp4');
 const PORT = 3333;
 const SERVER_HOST = 'localhost';
 
+function currentTime(){
+    return new Date().toLocaleString();
+}
+
 const message = 'Text to send to the server';
 
 client.send(message, PORT, SERVER_HOST, (err) => {
-    const currentTime = new Date().toLocaleString();
     if (err) {
-        console.error(`${currentTime} Client error: ${err}`);
+        console.error(`${currentTime()} Client error: ${err}`);
     }
     else {
-        console.log(`${currentTime} Message sent to server: ${message}`);
+        console.log(`${currentTime()} Message sent to server: "${message}"`);
     }
 });
 
 client.on('message', (msg, rinfo) => {
-    const currentTime = new Date().toLocaleString();
-    console.log(`${currentTime} Received response from server: ${msg}`);
+    console.log(`${currentTime()} Received response from server: "${msg}"`);
     client.close();
 });
