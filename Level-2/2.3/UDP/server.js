9679c11e9037b876ad49f89ee7257919116cfb5c
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2023-11-08 23:57:40 +0100

    Update server.js

--- Level-2/2.3/UDP/server.js
+++ Level-2/2.3/UDP/server.js
@@ -3,31 +3,34 @@ const server = dgram.createSocket('udp4');
 
 const PORT = 3333;
 
+function currentTime(){
+    return new Date().toLocaleString();
+}
+
 server.on('error', (err) => {
-    console.error(`Server error:\n${err.stack}`);
+    console.error(`${currentTime()}Server error:\n${err.stack}`);
     server.close();
 });
 
 server.on('message', (msg, rinfo) => {
-    const currentTime = new Date().toLocaleString();
-    console.log(`${currentTime} ${msg} from ${rinfo.address}:${rinfo.port}`);
+    const clientIP = rinfo.address;
+    console.log(`Client's IP-Adress : ${rinfo.address}`);
+    console.log(`${currentTime()} Recieved "${msg}" from ${rinfo.address}:${rinfo.port}`);
     const responseMessage = msg;
     server.send(responseMessage, rinfo.port, rinfo.address, (err) => {
         if (err) {
-            console.error(`${currentTime} Error sending response to client: ${err}`);
+            console.error(`${currentTime()} Error sending response to client: ${err}`);
         }
         else {
-            console.log(`${currentTime} Response sent to ${rinfo.address}:${rinfo.port}: ${responseMessage}`);
+            console.log(`${currentTime()} Response sent to ${rinfo.address}:${rinfo.port}: "${responseMessage}"`);
         }
     });
 });
 
-
 server.on('listening', () => {
     const address = server.address();
-    const currentTime = new Date().toLocaleString();
 
-    console.log(`${currentTime} UDP Server listening on ${address.address}:${address.port}`);
+    console.log(`${currentTime()} UDP Server listening on ${address.address}:${address.port}`);
 });
 
 server.bind(PORT);
