cd5015a2f7e3ea84b68989d38dac47a30ba6734f
AndrewSm299 <83510962+AndrewSm299@users.noreply.github.com>
2024-02-06 21:13:49 +0100

    done 3rd practice

---
+++ Level-3/3.1/src/practice3.ts
@@ -0,0 +1,36 @@
+// 1) сложный этап. напишите нормальную тайпскриптовую сигнатуру функции 
+// (отдельно опишите тип первого аргумента в виде interface)
+
+// https://pastebin.com/2nEJvk04
+
+
+interface isBigObject {
+    [key: string]: {
+            [key: string]: number | string | isBigObject
+        };
+    };
+
+let a: isBigObject = { hello: {cvalue: 1}, world: { cvalue: { yay: { cvalue: "2" } } } }
+
+function summ(a: isBigObject): number {
+    const x = Object.keys(a).map((k) => {
+        const elem = a[k];
+        if (typeof elem === undefined) return 2022;
+        if (typeof elem?.cvalue === 'string') return +elem.cvalue || 2022;
+        if (typeof elem?.cvalue == 'object') return summ(elem.cvalue);
+        else return elem?.cvalue;
+    });
+    let sum = 0;
+    for (let i = 0; i < x.length; i++) {
+        sum += x[i];
+    }
+    return sum;
+}
+
+console.log(summ(a))
+
+// - про guards: https://www.typescriptlang.org/docs/handbook/2/narrowing.html#typeof-type-guards
+// - про truthiness narrowing: https://www.typescriptlang.org/docs/handbook/2/narrowing.html#truthiness-narrowing
+// - про control flow analysis: https://www.typescriptlang.org/docs/handbook/2/narrowing.html#control-flow-analysis
+
+// Дайте знать @roman про результаты.
